a3275f6f584c47c49282c4439ea8f5d215964ea3
Peter Parkkali <peter.parkkali@iki.fi>
2016-05-26 20:31:49 +0300

    Added www/download-ext-deps.sh

---
+++ www/download-ext-deps.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 
+function get_github_package(){
+    ghuser="$1"
+    ghrepo="$2"
+    branch="$3"
+
+    url="https://github.com/${ghuser}/${ghrepo}/archive/${branch}.zip"
+    targetpath="public/ext/${ghrepo}"
+    tmpfile="tmpfile.zip"
+
+    echo "Installing ${url} into ${targetpath}"
+
+    err=""
+    [ -e "$targetpath" ] && {
+        echo "ERROR: targetpath ${targetpath} exists" >&2
+        err=true
+    }
+    [ -e "$tmpfile" ] && {
+        echo "ERROR: tempfile ${tmpfile} exists" >&2
+        err=true
+    }
+    [ "$err" == "true" ] && {
+        echo "Skipping this package" >&2
+        return
+    }
+        
+    curl -s -L -o "${tmpfile}" "$url"
+    unzip -q -d public/ext/ "${tmpfile}"
+    mv "public/ext/${ghrepo}-${branch}" "${targetpath}"
+    rm "${tmpfile}"
+}
+
+# Extended version of bs_grid (or clone/fork the repo and point www/public/ext/bs_grid to it)
+get_github_package ppar       bs_grid master
+
+# Dependencies for bs_grid
+get_github_package pontikis   bs_pagination master
+get_github_package pontikis   jui_filter_rules master
+
